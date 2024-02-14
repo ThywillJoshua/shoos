a31e59f8b6edf82a9e1cf97ccf2f9dd7ca87ce59
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-02-14 17:37:30 +0100

    Add yellow box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoesListPageComponent } from './products/containers/shoes-list-page/shoes-list-page.component';
 import { ShoePageComponent } from './products/containers/shoe-page/shoe-page.component';
+import { YellowBoxComponent } from './products/components/yellow-box/yellow-box.component';
+import { CartPageComponent } from './products/containers/cart-page/cart-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ShoesListPageComponent, ShoePageComponent],
+  declarations: [
+    AppComponent,
+    ShoesListPageComponent,
+    ShoePageComponent,
+    CartPageComponent,
+    YellowBoxComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/products/components/yellow-box/yellow-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yellow-box',
+  templateUrl: 'yellow-box.component.html',
+  styleUrls: ['./yellow-box.component.scss'],
+})
+export class YellowBoxComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
