6677855e87ef4d12ffbc9783b010c6f1017cb574
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-02-27 18:20:36 +0100

    Fix import error

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SharedRoutingModule } from './shared-routing.module';
-import { HomePageComponent } from './containers/home-page/home-page.component';
 import { NotFoundPageComponent } from './containers/not-found-page/not-found-page.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
 
 @NgModule({
   declarations: [HomePageComponent, NotFoundPageComponent],
